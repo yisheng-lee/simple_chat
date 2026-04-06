c73ddc41609b7cfb2ea02157296da667398d878f
YiSheng, Lee <yisheng@supahands.com>
2026-04-06 18:47:20 +1200

    Add generator for quick create Configuration

--- lib/simple_chat/configuration.rb
+++ lib/simple_chat/configuration.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
+# Usage in Rails application config/initializers/simple_chat.rb)
+# SimpleChat.configure do |config|
+#   config.chat_user_model = 'User'
+# end
 module SimpleChat
   class Configuration
     attr_accessor :chat_user_model
 
     def initialize
-      @chat_user_model = nil
+      @chat_user_model = "User"
     end
   end
 end
