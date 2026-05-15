71cdfe75726b6af501a5a9e132f226b82a0d7b7c
yisheng-lee <yisheng.lee@outlook.com>
2026-05-15 20:49:54 +1200

    Uses private channel for broadcasting message partials with local variables

--- app/models/simple_chat/message.rb
+++ app/models/simple_chat/message.rb
@@ -3,6 +3,19 @@ module SimpleChat
     belongs_to :chat_room
     belongs_to :user, class_name: SimpleChat.configuration.chat_user_model
 
-    after_create_commit -> { broadcast_append_later_to "simple_chat", chat_room, target: "messages_list", partial: "simple_chat/messages/message", locals: { message: self } }
+    after_create_commit :broadcast_to_members
+
+    private
+
+    def broadcast_to_members
+      chat_room.chat_members.each do |chat_member|
+        broadcast_append_later_to(
+          "simple_chat", chat_room, chat_member.user,
+          target: "messages_list",
+          partial: "simple_chat/messages/message",
+          locals: { message: self, current_user: chat_member.user }
+        )
+      end
+    end
   end
 end
